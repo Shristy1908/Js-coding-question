15ff5bffa95ba3d2d1cf29f3c9d0b3d011d0ffa3
Shristy1908 <shristykumari1908@gmail.com>
2025-07-06 20:52:03 +0530

    find range of integer optimized code

--- FindRangeOfInt.js
+++ FindRangeOfInt.js
@@ -1,13 +1,22 @@
-let arr=[21,56,2,57,34,58,21];
+let arr=[21,56,55,57,34,58,21];
 let low=51;
 let high=60;
-let result=[];
+ let result=[];
 
-for(let i=low; i<=high; i++){
-    for(let j=0;j<arr.length;j++){
-        if(i == arr[j]){
-            result.push(i);
-        }
-    }
+// for(let i=low; i<=high; i++){
+//     for(let j=0;j<arr.length;j++){
+//         if(i == arr[j]){
+//             result.push(i);
+//         }
+//     }
+// }
+// document.writeln(result)
+
+let set=new Set(arr);
+
+for(let i=low;i<=high;i++){
+  if( set.has(i)){
+      result.push(i);
+  }
 }
-document.writeln(result)
+document.writeln(result);
